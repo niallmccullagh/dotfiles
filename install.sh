3236085c9dd4042e5d39d0771505b948ac54b681
Niall McCullagh <niall@nialls-mbp.local>
2014-02-23 00:06:10 +0000

    Adding install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#ln -sf .git-prompt.sh ~/
+
+declare -a arr=(".aliases" ".bash_prompt" ".git" ".gitconfig" ".bash_profile" ".commonrc" ".git-prompt.sh")
+dest_dir=~
+cwd=$(pwd)
+
+echo $cwd
+
+pushd $dest_dir > /dev/null 2>&1
+
+for i in "${arr[@]}"
+do
+   echo "linking $dest_dir/$i to $cwd/$i"
+   ln -sf $cwd/$i $i
+done
+
+popd > /dev/null 2>&1
