78e5a305d5e5a77e4ea159507936ce4da9eda5ed
Niall McCullagh <niallmccullagh@users.noreply.github.com>
2019-08-29 20:37:55 +0100

    Commit vim submodules

--- .vim/install-bundles.sh
+++
@@ -1,6 +0,0 @@
-git submodule add https://github.com/bling/vim-airline .vim/bundle/vim-airline
-git submodule add git://github.com/altercation/vim-colors-solarized.git .vim/bundle/vim-colors-solarized
-git submodule add https://github.com/scrooloose/nerdtree.git .vim/bundle/nerdtree
-git submodule add git://github.com/tpope/vim-fugitive.git .vim/bundle/vim-fugitive
-git submodule add git://github.com/airblade/vim-gitgutter.git .vim/bundle/vim-gitgutter
-git submodule add https://github.com/editorconfig/editorconfig-vim.git
